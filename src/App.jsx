d6bfbe3073af9c78385bff90e57923bc60231847
Gabriel Basaglia <garbaroots@hotmail.com>
2022-06-27 09:24:46 -0300

    Autenticação

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './styles/global.css';
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
+import { AuthProvider } from './context/AuthContext';
 
 import { Home } from './pages/Home/Home';
 import { About } from './pages/About/About';
@@ -9,20 +14,37 @@ import { Footer } from './components/Footer';
 import { Register } from './pages/Register/Register';
 import { Login } from './pages/Login/Login';
 
+const [user, setUser] = useState(undefined);
+const { auth } = useAuthentication();
+
+const loadingUser = user === undefined;
+
+useEffect(() =>{
+  onAuthStateChanged(auth, user => {
+    setUser(user);
+  })
+},[auth])
+
+if(loadingUser) {
+  return <p>Carregando...</p>
+}
+
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="min-h-[60vh] mb-20">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
-      <Footer />
-    </BrowserRouter>
+    <div className="min-h-[60vh] mb-20">
+      <AuthProvider value={{user}}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
+    </div>
   );
 }
 
